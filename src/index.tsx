02c9d9ad8fbb807ff9d9366ff965fa3fe4560a44
Matt Tingen <matt.tingen@gmail.com>
2022-01-24 00:36:18 -0500

    Assert root element

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,9 @@
 import { render } from 'react-dom';
 import { Root } from './Root';
+import { assert } from './util/assert';
 
-const root = document.getElementById('root')!;
+const root = document.getElementById('root');
+
+assert(root, 'root element not found');
 
 render(<Root />, root);
