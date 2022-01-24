42cb374db50fa97bb8c455eafbf35cdc97db82c1
Matt Tingen <matt.tingen@gmail.com>
2022-01-24 00:02:29 -0500

    Add chakra-ui

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,8 +1,25 @@
+import { ChakraProvider } from '@chakra-ui/react';
+import createCache from '@emotion/cache';
+import { CacheProvider } from '@emotion/react';
 import { StrictMode } from 'react';
 import { App } from './App';
 
+// https://github.com/emotion-js/emotion/issues/1105#issuecomment-557726922
+const emotionCache = createCache({
+  key: 'root',
+  speedy: process.env.NODE_ENV !== 'development',
+  // Disable prefixes in dev env.
+  ...(process.env.NODE_ENV === 'development' && { stylisPlugins: [] }),
+});
+
+emotionCache.compat = true;
+
 export const Root = () => (
   <StrictMode>
-    <App />
+    <CacheProvider value={emotionCache}>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </CacheProvider>
   </StrictMode>
 );
